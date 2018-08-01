7c15545325b355acfeb81512a9181d7d9360faf3
yppgeorge <38196425+yppgeorge@users.noreply.github.com>
2018-08-01 00:58:03 -0400

    Comminting in a Rebasing effect

--- src/LinkedinLearningPath/Basics.java
+++ src/LinkedinLearningPath/Basics.java
@@ -4,9 +4,32 @@ public class Basics {
 
     public static void main(String[] Args){
         System.out.println("Contains the Basic Concepts");
+
+        primitiveDataTypes();
+
     }
 
 
+    public static void primitiveDataTypes(){
+        int myint = 5;
+        double mydouble = 156.5;
+        float myfloat = 10;
+        String myString = "Praveen George";
+
+        // WRAPPER CLASS EXAMPLES
+        String doublevalue = "156.5";
+        Double doubleObj = Double.parseDouble(doublevalue);
+
+        byte byteValue = doubleObj.byteValue();
+        int intValue = doubleObj.intValue();
+        float floatValue = doubleObj.floatValue();
+        String stringvalue = doubleObj.toString();
+
+        System.out.println("ByteValue" + byteValue);
+        System.out.println("IntValue" + intValue);
+        System.out.println("FloatValue" + floatValue);
+        System.out.println("String Value" + stringvalue);
+    }
 
 
 }
