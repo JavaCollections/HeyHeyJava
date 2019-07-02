ddecc79e2a2b8249b34fefcffa6e5c7760e4684c
Praveen George <c-praveen.george@charter.com>
2019-07-02 10:58:29 -0600

    OOPS CONCEPTS

--- src/LinkedinLearningPath_Learn/Basics_23_TryCatchWithCheckedAndUncheckedExceptions.java
+++ src/LinkedinLearningPath_Learn/Basics_23_TryCatchWithCheckedAndUncheckedExceptions.java
@@ -3,28 +3,47 @@ package LinkedinLearningPath_Learn;
 public class Basics_23_TryCatchWithCheckedAndUncheckedExceptions {
 
     public static void main(String[] args) {
+
         try {
-            String welcome = "Welcome!";
-            char[] chars = welcome.toCharArray();
-            char lastChar = chars[chars.length]; //EXCEPTION CREATED ArrayIndexOutOfBoundsException
-            System.out.println("Last Char: " + lastChar);
-
-            String nothing = null;
-            System.out.println(nothing.length());
-
+            String Welcome = "Welcome";
+            char[] chars = Welcome.toCharArray();
+            char lastchar = chars[Welcome.length() - 1];
+            System.out.println("Last Character is " + lastchar);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Array Index Exception");
             e.printStackTrace();
             return;
-        } catch (NullPointerException e) {
-            System.out.println("Null Pointer exception ");
-            e.printStackTrace();
-            return;
-        } catch (Exception e) {
-            System.out.println("Mother Exception Captured");
+        } catch (Exception e ) {
+            System.out.println("Any Exception");
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        // CALL TO DO SOIMETHING WITH AN EXCEPTION HANDLING FROM LINE 38
+        try {
+            doSomething();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
         System.out.println("Code ran Successfully");
     }
 
+
+    public static void doSomething() throws InterruptedException { // DECLARING THE EXCEPTION IN THE METHOD DECLARION, IN WHICH CASE WHEN CALLED THE EXCEPTION NEEDS TO BE HANDLED
+        Thread.sleep(1000);
+
+    }
+
 }
+//UNCHECKED EXCEPTION
+// ANY Exception Class which has RUNTIME EXCEPTION in its hierarchy is an unchecked exception,
+
+//CHECKED EXCEPTION
+// These need to be declared
+
---
+++ src/LinkedinLearningPath_Learn/Basics_OOPS_00_Encapsulation.java
@@ -0,0 +1,4 @@
+package LinkedinLearningPath_Learn;
+
+public class Basics_OOPS_00_Encapsulation {
+}
